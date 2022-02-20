afb0c9b13cd9ccd26bc3d88fdd5c711e92076a48
jiwon <scorpiusjwj@gmail.com>
2022-02-20 18:50:56 +0900

    [fix] test5

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const postsRouter = require("./routes/posts");
 
-mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("connected to Database"));
+// mongoose.connect(process.env.DATABASE_URL);
+// const db = mongoose.connection;
+// db.on("error", (error) => console.error(error));
+// db.once("open", () => console.log("connected to Database"));
 
 app.use("/posts", postsRouter);
 
