933f5bba8c392ad92957f35619357690ba665d19
jiwon <scorpiusjwj@gmail.com>
2022-02-20 18:17:59 +0900

    test

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const indexRouter = require("./routes/index");
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000, () => console.log("server on"));
