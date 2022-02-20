dadcfffbdcb1df97336a42ba7bb3fed1cc7d39a6
jiwon <scorpiusjwj@gmail.com>
2022-02-20 18:35:58 +0900

    [fix] 몸고db 끄고 다시 test

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
+// const cors = require("cors");
+// const mongoose = require("mongoose");
 const postsRouter = require("./routes/posts");
 
-mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("connected to Database"));
+// mongoose.connect(process.env.DATABASE_URL);
+// const db = mongoose.connection;
+// db.on("error", (error) => console.error(error));
+// db.once("open", () => console.log("connected to Database"));
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 app.use("/posts", postsRouter);
 
 app.listen(process.env.PORT || 3000, () => console.log("server on"));
